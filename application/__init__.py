0f7472542086b460659aaabf64e30fff95e22211
Daniel Koch <daxda@cs.helsinki.fi>
2018-08-09 19:25:35 +0300

    Määritettiin eri tietokanta herokuun sekä määritettiin ettei tietokantaa luoda aina uusiksi

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
 
 app.config["SQLALCHEMY_ECHO"] = True
@@ -34,4 +43,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
