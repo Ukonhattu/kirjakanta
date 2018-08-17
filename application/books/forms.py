3709c082ea1cfc6698b2fef26a4ca8b34a17c5d2
Daniel Koch <daxda@cs.helsinki.fi>
2018-08-17 23:38:07 +0300

    lisäsin genrepalkin max merkkimäärää huomattavasti, sillä tajusin että kun niitä voi laittaa useampia on 10 aivan liian vähän

--- application/books/forms.py
+++ application/books/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, BooleanField, validators
 
 class BookForm(FlaskForm):
     name  = StringField("Book name", [validators.Length(min=2, max=16)])
-    genres = StringField("Genres", [validators.Length(min=2, max=10)])
+    genres = StringField("Genres", [validators.Length(min=2, max=120)])
     read = BooleanField("Read")
 
     class Meta:
