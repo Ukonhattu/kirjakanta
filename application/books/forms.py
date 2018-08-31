6cf716b19ea6e6497fd655db44aa652399546b44
Daniel Koch <daxda@cs.helsinki.fi>
2018-08-31 23:59:29 +0300

    pidempiä kenttiä

--- application/books/forms.py
+++ application/books/forms.py
@@ -2,10 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, validators
 
 class BookForm(FlaskForm):
-    name  = StringField("Book name", [validators.Length(min=2, max=16)])
+    name  = StringField("Book name", [validators.Length(min=2, max=50)])
     genres = StringField("Genres", [validators.Length(min=2, max=120)])
     authors = StringField("Authors", [validators.Length(min=2, max=120)])
-    series  = StringField("Serie", [validators.Length(min=0, max=16)])
+    series  = StringField("Serie", [validators.Length(min=0, max=50)])
     read = BooleanField("Read")
 
     class Meta:
